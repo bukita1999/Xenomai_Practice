33a6199c3a3705d18bc9d7d7f398e597aefd27e7
bukita <choate1999@hotmail.com>
2022-11-10 12:44:19 +0100

    完善了文件的结构体，并且把测试用的与实际的相分开

--- simple_full_rt/Output_data.h
+++ simple_full_rt/Output_data.h
@@ -1,12 +1,17 @@
 #include <time.h>
 
 
-struct Output_data{
+typedef struct Output_data{
+    int ctr;
     char timestamp[40]; //时间戳字符化后的效果
     float pulse_increment; //脉冲增量实际对比，此处是真实一个脉冲增量对应的距离
     int x_position; //目前所走到的位置x坐标
     int y_position;  //目前所走到的位置y坐标
+    int x_step; //x轴是否前进，前进为1，未前进则为0
+    int y_step; //y轴是否前进，前进为1，未前进则为0
     int real_cycle_time; //实际周期时间是运行平台可能实时性并不足够好，可能会有相应的波动
     int real_intepolation_time; //由于实施周期平台不一定足够好
     int intrp_prop_on_cycle; //差补时间占周期时间的比例
-};
+}Output_data;
+
+extern struct Output_data;
--- simple_full_rt/simple_rt.c
+++ simple_full_rt/simple_rt.c
@@ -12,11 +12,13 @@
 #define LOOP_PERIOD 1e7
 
 RT_TASK loop_task;
+struct Output_data;
 
 //输出的方式可以时间戳写在文件当中，也可以通过usb连GPIO的方式进行输出
-int output_csv_single_pulse(FILE *file) 
+int output_csv_single_pulse(FILE *file, Output_data * output) 
 {
-
+  fprintf(file,"");
+  fprintf(file,"");
 }
 
 int dda_line_intepolation(float start_x, float start_y, float end_x, float end_y, float single_step) // dda开环控制
@@ -33,6 +35,7 @@ int dda_line_intepolation(float start_x, float start_y, float end_x, float end_y
   curtask = rt_task_self();
   //获取此时时钟的指针
   rt_task_inquire(curtask, &curtaskinfo);
+  //设置计时器
   int ctr = 0;
 
   while (1)
@@ -77,16 +80,29 @@ int main(int argc, char **argv)
 {
   char str[20];
 
-  mlockall(MCL_CURRENT | MCL_FUTURE); //锁定内存相关内容，以防止Page交换引起大量延时
+  //锁定内存相关内容，以防止Page交换引起大量延时
+  mlockall(MCL_CURRENT | MCL_FUTURE); 
 
   printf("Starting cyclic task...\n");
 
+  //创建文件指针，准备将带有时间戳的文件写入CSV文件当中去
+  FILE * file = fopen("output.csv","w");
+  //写csv文件的标题
+  fprintf(file,"ID,TimeStamp,PulseSize,X,Y,x_step,y_step,Cycle Time,Intepolation Time,Intepolation Porprotion");
+
+
   sprintf(str, "cyclic_task");
+  //创建实时任务
   rt_task_create(&loop_task, str, 0, 50, 0);
 
-  rt_task_start(&loop_task, &dda_line_intepolation, 0);
+  //将需要使用的代码进行注释，尽量保证内外不影响
+  // rt_task_start(&loop_task, &dda_line_intepolation, 0);
+
+  rt_task_start(&loop_task, &loop_task_proc, 0);
 
   pause();
 
+
+  fclose(file);
   return 0;
 }
