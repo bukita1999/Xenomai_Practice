0413adf7241c2c7fe51f85b596eafe3a0c8939b6
bukita <choate1999@hotmail.com>
2022-11-10 11:47:32 +0100

    对于simple_rt.c进行了写入

--- simple_full_rt/simple_rt.c
+++ simple_full_rt/simple_rt.c
@@ -8,39 +8,42 @@
 
 #include "Output_data.h"
 
-
 #define CLOCK_RES 1e-9
 #define LOOP_PERIOD 1e7
 
 RT_TASK loop_task;
 
-enum OUTPUT_MODE
-{
-    GPIO_MODE,FILE_MODE,
-};
-
-
-
-int output_file_single_pulse(char * file_str) //输出的方式可以时间戳写在文件当中，也可以通过usb连GPIO的方式进行输出
+//输出的方式可以时间戳写在文件当中，也可以通过usb连GPIO的方式进行输出
+int output_csv_single_pulse(FILE *file) 
 {
 
 }
 
-int dda_line_intepolation(float start_x,float start_y,float end_x, float end_y,float single_step) //dda开环控制
+int dda_line_intepolation(float start_x, float start_y, float end_x, float end_y, float single_step) // dda开环控制
 {
+  RT_TASK *curtask;
   RTIME tstart, now;
-  int ctr = 0;
+  RT_TASK_INFO curtaskinfo;
+  int iret = 0;
+
+  RTIME tstart, now;
 
   rt_task_set_periodic(NULL, TM_NOW, LOOP_PERIOD);
 
-  tstart = rt_timer_read();
+  curtask = rt_task_self();
+  //获取此时时钟的指针
+  rt_task_inquire(curtask, &curtaskinfo);
+  int ctr = 0;
 
-  while(1){
-    printf("Loop count: %d, Loop time: %.5f ms\n", ctr, (rt_timer_read() - tstart)/1000000.0);
+  while (1)
+  {
+    printf("Loop count: %d, Loop time: %.5f ms\n", ctr, (rt_timer_read() - tstart) / 1000000.0);
     ctr++;
     rt_task_wait_period(NULL);
   }
 }
+
+
 
 void loop_task_proc(void *arg)
 {
@@ -52,39 +55,38 @@ void loop_task_proc(void *arg)
 
   curtask = rt_task_self();
   rt_task_inquire(curtask, &curtaskinfo);
-  
+  int ctr = 0;
 
-
+  //Print the info
   printf("Starting task %s with period of 10 ms ....\n", curtaskinfo.name);
 
-
+  //Make the task periodic with a specified loop period
   rt_task_set_periodic(NULL, TM_NOW, LOOP_PERIOD);
 
   tstart = rt_timer_read();
 
-
-  
+  //Start the task loop
+  while(1){
+    printf("Loop count: %d, Loop time: %.5f ms\n", ctr, (rt_timer_read() - tstart)/1000000.0);
+    ctr++;
+    rt_task_wait_period(NULL);
+  }
 }
 
 int main(int argc, char **argv)
 {
   char str[20];
 
-
-  mlockall(MCL_CURRENT | MCL_FUTURE);
+  mlockall(MCL_CURRENT | MCL_FUTURE); //锁定内存相关内容，以防止Page交换引起大量延时
 
   printf("Starting cyclic task...\n");
-
 
   sprintf(str, "cyclic_task");
   rt_task_create(&loop_task, str, 0, 50, 0);
 
-
-  rt_task_start(&loop_task, &loop_task_proc, 0);
-
+  rt_task_start(&loop_task, &dda_line_intepolation, 0);
 
   pause();
 
   return 0;
 }
-
